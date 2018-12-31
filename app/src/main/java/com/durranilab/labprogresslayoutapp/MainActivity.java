9cc3147182966ec70476a1268362fb5c352cb06f
Musaddiq <admin@tuxads.com>
2018-12-31 23:14:45 +0530

    First Release

---
+++ app/src/main/java/com/durranilab/labprogresslayoutapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.durranilab.labprogresslayoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.durranilab.labprogresslayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
